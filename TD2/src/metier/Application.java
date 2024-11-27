f21ed40c043bd4c0ca3aadfb26205feef12409d0
FayssoilSaid <sfayssoil8@gmail.com>
2024-11-27 18:50:15 +0100

    --

--- TD2/src/metier/Application.java
+++
@@ -1,9 +0,0 @@
-package metier;
-
-public class Application {
-	public static void main(String[] args) {
-		Compte c1 = new Compte(0000001,0);
-		c1.setSolde(1000);
-		System.out.println("Informations de votre compte: "+c1);
-	}
-}
